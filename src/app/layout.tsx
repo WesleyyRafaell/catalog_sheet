c7d35ccd5516aef4e4bfd321ea2a0bbc338a0579
wess <wesley.santos@mblabs.com.br>
2024-10-03 17:26:36 -0300

    feat: enableSystem false

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout ({
         <ThemeProvider
           attribute='class'
           defaultTheme='light'
+          enableSystem={false}
         >
           {children}
         </ThemeProvider>
