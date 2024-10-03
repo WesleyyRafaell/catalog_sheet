f7abc1e6bcccf04b3d084fb6861a8c2c7d079bb6
wess <wesley.santos@mblabs.com.br>
2024-10-03 17:20:59 -0300

    feat: light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,6 @@ export default function RootLayout ({
         <ThemeProvider
           attribute='class'
           defaultTheme='light'
-          enableSystem
-          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
